8c2fd260a6969d64a4ecd3c6817b4656262c0005
matheus73 <matheuscoding@gmail.com>
2020-11-11 12:03:12 -0300

    global

--- front/src/components/Navbar/index.js
+++ front/src/components/Navbar/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Nav from './styles';
 
+import Input from '../Input';
 import Button from '../Button';
 import Item from '../Item';
 
 function Navbar(props){
     return (
         <Nav>
+            <Input/>
             <Item white mobileOff>Sobre</Item>
             <Item white>Entrar</Item>
             <Button second>CADASTRA-SE</Button>
