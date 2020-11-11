097e739607cb3710f0b589a4c6b99e6dd3173392
matheus73 <matheuscoding@gmail.com>
2020-11-11 13:09:24 -0300

    Revert "global"
    This reverts commit 8c2fd260a6969d64a4ecd3c6817b4656262c0005.

--- front/src/components/Navbar/index.js
+++ front/src/components/Navbar/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Nav from './styles';
 
-import Input from '../Input';
 import Button from '../Button';
 import Item from '../Item';
 
 function Navbar(props){
     return (
         <Nav>
-            <Input/>
             <Item white mobileOff>Sobre</Item>
             <Item white>Entrar</Item>
             <Button second>CADASTRA-SE</Button>
