b0953cf112397c8558f94f2c53b9d08a59f14d9f
matheus73 <matheuscoding@gmail.com>
2020-11-11 13:14:08 -0300

    revertendo tudo

--- front/src/styles/global.js
+++ front/src/styles/global.js
@@ -2,35 +2,6 @@ import {createGlobalStyle} from 'styled-components';
 import trianguloBackground from '../assets/triangulobackground.svg';
 
 export default createGlobalStyle`
-<<<<<<< HEAD
-*{
-    margin:0;
-    margin-top: 0;
-    border: 0;
-    padding: 0;
-    outline: 0;
-    box-sizing: border-box;
-}
-body {
-    background: #E5E5E5 url(${trianguloBackground}) no-repeat top;
-    -webkit-font-smoothing: antialiased;
-}
-body, input, button {
-    font: 16px Roboto, sans-serif;
-}
-#root {
-    max-width: 970px;
-    margin: 0 auto;
-    padding: 40px 20px;
-}
-button {
-    cursor: pointer;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
-=======
     *{
         margin:0;
         border: 0;
@@ -60,5 +31,4 @@ table {
         border-collapse: collapse;
         border-spacing: 0;
     }
->>>>>>> e721deda98f01210d6ba7aa1a891096ec619de82
 `;
