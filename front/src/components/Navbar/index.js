4f8f09109858fd15acb3df1c7938c880b916c12c
matheus73 <matheuscoding@gmail.com>
2020-11-11 13:06:15 -0300

    Revert "Tentando resolver o problema que eu criei"
    This reverts commit 9b762882fdd8f6a63bb866a38423c42f7fbf2e3c.

---
+++ front/src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Nav from './styles';
+
+import Input from '../Input';
+import Button from '../Button';
+import Item from '../Item';
+
+function Navbar(props){
+    return (
+        <Nav>
+            <Input type="password" name="putaria" placeholder="AHHHHH"/>
+            <Item white mobileOff>Sobre</Item>
+            <Item white>Entrar</Item>
+            <Button second>CADASTRA-SE</Button>
+        </Nav>
+    );
+}
+
+export default Navbar;
